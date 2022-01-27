e7228181a037f128201c74a273afc8713510d1d0
Vojtěch Havránek <vojtahavranek@gmail.com>
2022-01-27 14:09:48 +0100

    Data attributes for textareas

---
+++ src/NetteHtmlDataAttributesProvider.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\Components\NetteForm;
+
+use BrandEmbassy\Components\DataAttribute;
+use BrandEmbassy\Components\DataAttributes;
+use Nette\Utils\Html;
+use Nette\Utils\Strings;
+use function array_filter;
+use function is_array;
+
+final class NetteHtmlDataAttributesProvider
+{
+    /**
+     * @var Html
+     */
+    private $html;
+
+
+    public function __construct(Html $html)
+    {
+        $this->html = $html;
+    }
+
+
+    public function findDataAttributes(): ?DataAttributes
+    {
+        $rawDataAttributes = array_filter($this->html->attrs, static function (string $key): bool {
+            return Strings::startsWith($key, 'data-');
+        }, ARRAY_FILTER_USE_KEY);
+
+        if ($rawDataAttributes === []) {
+            return null;
+        }
+
+        $dataAttributes = [];
+
+        foreach ($rawDataAttributes as $attributeName => $attributeValue) {
+            if (is_array($attributeValue)) {
+                $dataAttributes[] = DataAttribute::createWithArrayValue($attributeName, $attributeValue);
+                continue;
+            }
+
+            $dataAttributes[] = DataAttribute::createWithStringValue($attributeName, (string)$attributeValue);
+        }
+
+        return new DataAttributes($dataAttributes);
+    }
+}
---
+++ src/NetteHtmlDataAttributesProviderTest.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+
+namespace BrandEmbassy\Components\NetteForm;
+
+use Nette\Utils\Html;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+final class NetteHtmlDataAttributesProviderTest extends TestCase
+{
+    private const EXPECTED_DATA_ATTRIBUTES_HTML = 'data-first=\'string-value\' data-second=\'{"json":"value"}\'';
+
+
+    public function testFindingDataAttributes(): void
+    {
+        $html = new Html();
+        $html->addAttributes([
+            'data-first' => 'string-value',
+            'style' => 'text-align: center;',
+            'data-second' => [
+                'json' => 'value',
+            ],
+        ]);
+
+        $attributesResolver = new NetteHtmlDataAttributesProvider($html);
+        $dataAttributes = $attributesResolver->findDataAttributes();
+
+        assert($dataAttributes !== null);
+        Assert::assertSame(self::EXPECTED_DATA_ATTRIBUTES_HTML, $dataAttributes->getHtmlAttributes());
+    }
+
+
+    public function testReturningNullWhenNoDataAttributeFound(): void
+    {
+        $html = new Html();
+        $html->addAttributes([
+            'style' => 'text-align: center;',
+            'height' => 300,
+        ]);
+
+        $attributesResolver = new NetteHtmlDataAttributesProvider($html);
+        $dataAttributes = $attributesResolver->findDataAttributes();
+
+        Assert::assertNull($dataAttributes);
+    }
+}
